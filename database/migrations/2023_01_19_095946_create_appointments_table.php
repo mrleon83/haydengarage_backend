c9ec9e2fe337051ee45bf554fdcf3932d3d22c94
Leon Kimpton <leon010983@yahoo.co.uk>
2023-01-20 09:31:52 +0000

    Initial Commit

---
+++ database/migrations/2023_01_19_095946_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_id');
+            $table->string('day');
+            $table->string('month');
+            $table->string('year');
+            $table->string('time');
+            $table->string('date_string');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
